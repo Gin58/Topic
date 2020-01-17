6f77b1e128e9d0d0745b02ebab04c92c10306a65
Gin58 <invincible.gnj1875@gmail.com>
2020-01-18 01:47:38 +0900

    first commit

---
+++ src/app/Topic.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Topic extends Model
+{
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+}
